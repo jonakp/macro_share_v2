2670e4953699ee16be0278fe4e375b61d3247cc5
kpb <email@example.com>
2018-10-27 16:59:52 +0900

    Issue#2 Re-Fix migration file of create user

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_27_001305) do
+ActiveRecord::Schema.define(version: 2018_10_27_075759) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "email"
+    t.string "email", default: "", null: false
     t.integer "gender"
     t.string "image_name"
     t.datetime "created_at", null: false
