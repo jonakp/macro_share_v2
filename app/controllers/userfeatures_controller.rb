deceeda5f72e2ea9785270f314d8e8f899c19605
Takahiro Masui <mst2253@gmail.com>
2018-11-13 18:50:35 +0900

    revert [WIP]Issue #16 fix userfeature form by nested in edit

--- app/controllers/userfeatures_controller.rb
+++ app/controllers/userfeatures_controller.rb
@@ -40,10 +40,11 @@ class UserfeaturesController < ApplicationController
   # PATCH/PUT /userfeatures/1
   # PATCH/PUT /userfeatures/1.json
   def update
-    @user = current_user
+    @userfeature.attributes = userfeature_params
+    @userfeature.user.gender = userfeature_user_params[:gender]
 
     respond_to do |format|
-      if @user.update(nested_user_params)
+      if @userfeature.update(userfeature_params) && @userfeature.user.update(userfeature_user_params)
         format.html { redirect_to @userfeature.user, notice: 'Userfeature was successfully updated.' }
         format.json { render :show, status: :ok, location: @userfeature }
       else
@@ -74,7 +75,7 @@ class UserfeaturesController < ApplicationController
       params.require(:user).permit(
         :name,
         :gender,
-        userfeatures_attributes: [:id, :height, :weight, :age, :activity, :purpose]
+        userfeatures_attributes: [:height, :weight, :age, :activity, :purpose]
       )
     end
 end
