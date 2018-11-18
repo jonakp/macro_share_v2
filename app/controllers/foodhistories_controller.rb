0226bd404a490794354b37dcbf4fe1b4bc92b3ae
Takahiro Masui <mst2253@gmail.com>
2018-11-19 01:20:49 +0900

    Issue #21 Fix the logic of search

--- app/controllers/foodhistories_controller.rb
+++ app/controllers/foodhistories_controller.rb
@@ -6,7 +6,6 @@ class FoodhistoriesController < ApplicationController
   def index
     @q = Foodhistory.ransack(params[:q])
     @foodhistories = @q.result
-    # @foodhistories2 = @q.result(distinct: true).select('name')
   end
 
   # GET /foodhistories/1
@@ -16,14 +15,8 @@ class FoodhistoriesController < ApplicationController
 
   # GET /foodhistories/new
   def new
-    # スタンダードな実装？もうちょっとスマートにしたい
-    # if params[:reuse_record_id]
-    #   @foodhistory = Foodhistory.find(params[:reuse_record_id]).dup
-    # else
-    #   @foodhistory = Foodhistory.new
-    # end
-    @foodhistory = Foodhistory.find_by(id: params[:reuse_record_id]).dup
-    @foodhistory ||= Foodhistory.new
+    @foodhistory = params[:reuse_record_id] ?
+     Foodhistory.find(params[:reuse_record_id]).dup : Foodhistory.new
   end
 
   # GET /foodhistories/1/edit
