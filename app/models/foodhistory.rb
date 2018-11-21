e92d46d1f0733018c5778513db90800f2496233e
Takahiro Masui <mst2253@gmail.com>
2018-11-22 07:56:46 +0900

    Issue #25 Clean the condition of foodhistories index output

--- app/models/foodhistory.rb
+++ app/models/foodhistory.rb
@@ -2,6 +2,7 @@ class Foodhistory < ApplicationRecord
   belongs_to :user
   enum mode: { pri: 0, pub: 1 }
 
+  # 以下の2つのメソッドは上手く1つにまとめる方法はないか、要検討
   def can_access?(current_user)
     pub? || current_user.id == user.id
   end
