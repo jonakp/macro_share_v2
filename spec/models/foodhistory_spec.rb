4c9953b3dd654625f10eb86b37c7b94ecd753cb4
Takahiro Masui <mst2253@gmail.com>
2018-12-21 07:40:40 +0900

    Issue #33 modify rspec for Notification & UserMailer

--- spec/models/foodhistory_spec.rb
+++ spec/models/foodhistory_spec.rb
@@ -11,12 +11,16 @@ RSpec.describe Foodhistory, type: :model do
 
     it { expect(Like.by_user_and_foodhistory(user, foodhistory).exists?).to be true }
     it { expect(foodhistory).to be_liked_by(user) }
+    it { expect(Notification.where(user: foodhistory.user).size).to eq(1) }
+    it { expect(ActionMailer::Base.deliveries.size).to eq(1) }
 
     context 'when already liked' do
       it 'should be able to unlike' do
         foodhistory.unlike(user)
         expect(Like.by_user_and_foodhistory(user, foodhistory).exists?).not_to be true
         expect(foodhistory).not_to be_liked_by(user)
+        # 以下は暫定のexample。Issue#37で修正される予定
+        expect(Notification.where(user: foodhistory.user).size).to eq(0)
       end
     end
   end
