9791a866e95adee62e9c8414d8312a0f8a771056
Takahiro Masui <mst2253@gmail.com>
2018-12-21 07:27:53 +0900

    Issue #33 modify rspec by using describe & context

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     sequence(:name) { |n| "test#{n}" }
     sequence(:email) { |n| "test#{n}@me.com" }
-    gender :male
-    password "123456"
+    gender { :male }
+    password { "123456" }
   end
 end
--- spec/models/foodhistory_spec.rb
+++ spec/models/foodhistory_spec.rb
@@ -4,13 +4,28 @@ RSpec.describe Foodhistory, type: :model do
   let(:user) { create(:user) }
   let(:foodhistory) { create(:foodhistory) }
 
-  it "should be able to do like-unlike" do
-    expect(foodhistory).not_to be_liked_by(user)
+  describe "#like" do
+    before do
+      foodhistory.like(user)
+    end
 
-    foodhistory.like(user)
-    expect(foodhistory).to be_liked_by(user)
+    it { expect(Like.by_user_and_foodhistory(user, foodhistory).exists?).to be true }
+    it { expect(foodhistory).to be_liked_by(user) }
 
-    foodhistory.unlike(user)
-    expect(foodhistory).not_to be_liked_by(user)
+    context 'when already liked' do
+      it 'should be able to unlike' do
+        foodhistory.unlike(user)
+        expect(Like.by_user_and_foodhistory(user, foodhistory).exists?).not_to be true
+        expect(foodhistory).not_to be_liked_by(user)
+      end
+    end
+  end
+
+  describe "#unlike" do
+    it 'should not be able to unlike to unliked foodhistory' do
+      expect {
+        foodhistory.unlike(user)
+      }.to raise_error(ActiveRecord::RecordNotFound)
+    end
   end
 end
